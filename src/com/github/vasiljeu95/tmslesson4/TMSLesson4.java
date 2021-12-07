269151a69edbe35008408ae1b6e8cce9ac4d0824
vasiljeu95 <vasilyeu95@gmail.com>
2021-12-07 19:18:38 +0300

    firstExample

--- src/com/github/vasiljeu95/tmslesson4/TMSLesson4.java
+++ src/com/github/vasiljeu95/tmslesson4/TMSLesson4.java
@@ -1,7 +1,12 @@
 package com.github.vasiljeu95.tmslesson4;
 
+import java.util.Scanner;
+
 public class TMSLesson4 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число");
+        int a = sc.nextInt();
+        System.out.println("Вы ввели "+a);
     }
 }
